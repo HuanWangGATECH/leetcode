4f7fcc3b84ff676cd53b6a80b9f654394ac73f25
Huan Wang <56770390+HuanWangGATECH@users.noreply.github.com>
2021-08-24 00:05:21 -0700

    Update 1.triple_hop.py

--- career_cup/1.triple_hop.py
+++ career_cup/1.triple_hop.py
@@ -26,3 +26,49 @@ if __name__ == "__main__":
     print(method_2(4))
     print(method_2(5))
     print(method_2(6))
+    
+
+    
+    ### leetcode 70 solution 
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        
+        
+        if n<0: return 0
+        if n==1: return 1
+        if n==2: return 2
+      
+            
+        result=[0]*(n+1)
+        result[0]=0
+        result[1]=1
+        result[2]=2
+      
+        for i in range(3,n+1):
+            
+            #print (i)
+            result[i]=result[i-1]+result[i-2]
+            
+        return result[n]
+  
+
+### leetcode 70 using dictionary solution 
+    
+   class Solution:
+    def climbStairsrec(self, num: int) -> int:
+            if num == 1:
+                return 1
+            if num == 2:
+                return 2
+        
+            if num in self.dct:
+                return self.dct[num]
+            else:
+                self.dct[num] = self.climbStairsrec(num-1) + self.climbStairsrec(num-2)  
+                
+            return self.dct[num]
+    
+    
+    def climbStairs(self, n: int) -> int:
+        self.dct = {}
+        return self.climbStairsrec(n)
