7fc43c49fda9388f722f1899f056baeaa7080bad
Huan Wang <56770390+HuanWangGATECH@users.noreply.github.com>
2021-09-11 07:38:26 -0700

    Create lc56.merge_interval.py

---
+++ amazon_top100/lc56.merge_interval.py
@@ -0,0 +1,25 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        sorted(intervals,key=lambda x:x[0])
+        start=intervals[0][0]
+        end=intervals[0][1]
+        result=[]
+        for i in range(1,len(intervals)):
+           
+            interval=intervals[i]
+            currStart=interval[0]
+            currEnd=interval[1]
+            print (start,end,currStart,currEnd)
+            if currStart<=end:
+                end=max(end,currEnd)
+            else:
+                print ("append")
+                result.append([start,end])
+                start=currStart
+                end=currEnd
+                
+        result.append([start,end])        
+        return result 
+        
+        
+        
