3057ab0f1a0ba75e7e7d45371a7d61b4920246b0
Huan Wang <56770390+HuanWangGATECH@users.noreply.github.com>
2021-11-01 22:48:28 -0700

    Update leetcode1136.parallel_courses.py

--- leetcode_my_owntag/graph/topological_sort/leetcode1136.parallel_courses.py
+++ leetcode_my_owntag/graph/topological_sort/leetcode1136.parallel_courses.py
@@ -50,3 +50,37 @@ class Solution:
             return -1
             
      ### DFS solution still need some time  
+    
+    
+    def minimumSemesters(self, N, relations):
+    """
+    :type N: int
+    :type relations: List[List[int]]
+    :rtype: int
+    """
+    n = N        
+    d = collections.defaultdict(list)
+    visited = [0 for _ in range(n + 1)]
+    depth = [1 for _ in range(n + 1)]
+    for x, y in relations:
+        d[y].append(x)
+    for i in range(1, n + 1):
+        if not self.dfs(i, d, visited, depth, 1):
+            return -1
+    return max(depth)
+
+def dfs(self, i, d, visited, depth, cnt):
+    if visited[i] == 1:
+        return False
+    if visited[i] == 2:
+        return True
+    visited[i] = 1
+    for j in d[i]:
+        depth[i] = max(depth[i], depth[j] + 1)
+        if not self.dfs(j, d, visited, depth, cnt):
+            return False
+    visited[i] = 2
+    return True
+
+
+
