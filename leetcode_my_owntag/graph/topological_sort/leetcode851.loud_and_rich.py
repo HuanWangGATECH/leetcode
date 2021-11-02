d87f223b141a166a1d3cd5c23424a84e9ff217cc
Huan Wang <56770390+HuanWangGATECH@users.noreply.github.com>
2021-11-02 13:37:32 -0700

    Create leetcode851.loud_and_rich.py

---
+++ leetcode_my_owntag/graph/topological_sort/leetcode851.loud_and_rich.py
@@ -0,0 +1,31 @@
+from collections import deque
+class Solution:
+    def loudAndRich(self, richer: List[List[int]], quiet: List[int]) -> List[int]:
+        n=len(quiet)
+        ans=[-1]*n
+        
+        graph={}
+        
+        for i in range(n):
+            graph[i]=[]
+        
+        
+        for u,v in richer:
+            graph[v].append(u)
+            
+            
+        for u in range(n):
+            if ans[u]==-1:
+                self.dfs(u,graph,ans,quiet)
+    
+        return ans
+    
+    def dfs(self,u,graph,ans,quiet):
+        
+        ans[u]=u
+        for v in graph[u]:
+            self.dfs(v,graph,ans,quiet)
+            if quiet[ans[v]] < quiet[ans[u]]:
+                ans[u]=ans[v]
+        
+        
