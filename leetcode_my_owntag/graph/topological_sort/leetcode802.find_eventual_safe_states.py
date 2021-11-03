a2af6ed7b81a5538e4a3943b6e34b4be85341893
Huan Wang <56770390+HuanWangGATECH@users.noreply.github.com>
2021-11-03 12:29:52 -0700

    Update leetcode802.find_eventual_safe_states.py

--- leetcode_my_owntag/graph/topological_sort/leetcode802.find_eventual_safe_states.py
+++ leetcode_my_owntag/graph/topological_sort/leetcode802.find_eventual_safe_states.py
@@ -42,4 +42,41 @@ class Solution:
             
         return sorted(result)
                 
+   ## DFS solution 
+
+
+from collections import deque 
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+      
+        
+        
+        
+        n=len(graph)
+        visited=[0]*n
+        res=[]
+        
+        for i in range(n):
+            if not self.dfs(i,graph,visited):
+                res.append(i)
+         
+        return res 
+    
+    def dfs(self,u,graph,visited):
+        # return True if cycle detected 
+        if visited[u]==1:
+            return True 
+        if visited[u]==2:
+            return False 
+        
+        visited[u]=1
+        
+        for v in graph[u]:
+            if self.dfs(v,graph,visited):
+                return True 
+        
+        visited[u]=2
+        return False 
+                
+        
         
